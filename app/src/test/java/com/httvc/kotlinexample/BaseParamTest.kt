a2007f15986fd689d2936e8b87f3ab07bb7eb2dc
httvc <923351919@qq.com>
2017-06-19 16:50:15 +0800

    提交基本类型学完

--- app/src/test/java/com/httvc/kotlinexample/BaseParamTest.kt
+++ app/src/test/java/com/httvc/kotlinexample/BaseParamTest.kt
@@ -190,6 +190,113 @@ class BaseParamTest {
         val price=""" ${'$'}9.99"""
         println("价格为$price")
 
+        //if 表达式
+        //在Kotlin中，if是一个表达式，即它会返回一个值。因此就不需要三元运算符，普通if就可以
+
+        var max=a
+        if(a<b) max=b
+
+        var max1: Int
+        if(a>b){
+            max=a
+        }else{
+            max=b
+        }
+
+        //表达式
+        val max2=if(a>b) a else b
+
+        //if的分支可以是代码块，最后的表达式作为该块的值
+
+        val max3=if(a>b){
+            println("Choose a")
+            a
+        }else{
+            println("Choose b")
+            b
+        }
+
+        println(max3)
+        //如果你使用 if 作为表达式而不是语句（例如：返回它的值或者把它赋给变量），该表达式需要有 else 分支
+
+        //when 表达式
+       // when 取代了类C语言的switch操作符。其最简单的形式如下：
+        val p: Int ?=3
+        when(p){
+            1-> println("x==1")
+            2-> println("x==2")
+            else->{//注意这个块
+                println("x is neither 1 nor 2")
+            }
+        }
+
+       //如果很多分支需要用相同的方式处理，则可以把多个分支条件放在一起，用逗号分隔
+        when(p){
+            0,1-> println("x==0 or x==1")
+            else-> println("otherwise")
+        }
+
+       // 我们也可以检测一个值在（in）或者不在（!in）一个区间或者集合中：
+        when(p){
+            in 1..10-> println("x is in 1 to 10 the range")
+            !in 10..20-> println("x is outside 10 to 20 the range")
+            else -> println("none of the above")
+        }
+
+        //检测一个值是（is）或者不是（!is）一个特定类型的值
+        //你可以访问该类型的方法和属性而无需任何额外的检测
+        val s="1"
+        when(s){
+            is String -> s.startsWith("prdex")
+            else -> false
+        }
+
+        //when 也可以用来取代 if-else if链。
+        // 如果不提供参数，所有的分支条件都是简单的布尔表达式，而当一个分支的条件为真时则执行该分支
+       /* when {
+            s::isOdd() -> print("x is odd")
+            x.isEven() -> print("x is even")
+            else -> print("x is funny")
+        }*/
+
+        //Kotlin 也有无装箱开销的专门的类来表示原生类型数组: ByteArray、 ShortArray、IntArray 等等。
+        //这些类和 Array 并没有继承关系，但是它们有同样的方法属性集。它们也都有相应的工厂方法:
+        val array: IntArray = intArrayOf(1,2,3,4,5)
+        val array1: ByteArray = byteArrayOf(1,2,3,4)
+        val asc=Array(5,{i->(i*i).toString()})
+        for (item in array.iterator()){
+            println(item)
+        }
+
+        /**
+         * return 默认从最直接包围它的函数或者匿名函数返回
+         *
+         * break 终止最直接包围它的循环
+         *
+         * continue 继续下一次最直接包围它的循环
+         */
+
+        //Break 和 Continue 标签
+        //在 Kotlin 中任何表达式都可以用标签（label）来标记。 标签的格式为标识符后跟 @ 符号
+        //标签限制的 break 跳转到刚好位于该标签指定的循环后面的执行点。 continue 继续标签指定的循环的下一次迭代。
+        loop@ for(i in 1..100){
+            for (j in 1..20){
+                if (i==j){
+                    println("$i------$j")
+                    continue@loop
+                }else{
+                    println("$i------$j")
+                    break@loop
+                }
+            }
+        }
+
+
+
     }
+    //标签处返回
+    //直接返回  标签限制的 return 允许我们从外层函数返回。
+    //返回到一层 加@
+
 
 }
